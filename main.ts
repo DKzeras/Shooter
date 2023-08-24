cb6c4a7626aecc282a1a004fcdf40e48c979bc3f
DKzeras <143094613+DKzeras@users.noreply.github.com>
2023-08-24 09:53:22 -0300

    Minha marca (Favor não retirar)

--- main.ts
+++ main.ts
@@ -1,35 +1,35 @@
 input.onButtonPressed(Button.A, function () {
-    if (canShoot) {
-        Bullet = game.createSprite(Spaceship.get(LedSpriteProperty.X), Spaceship.get(LedSpriteProperty.Y))
-        Bullet.turn(Direction.Left, 50)
-        canShoot = false
+    if (canShootDKzeras) {
+        BulletDkzeras = game.createSprite(SpaceshipDkzeras.get(LedSpriteProperty.X), SpaceshipDkzeras.get(LedSpriteProperty.Y))
+        BulletDkzeras.turn(Direction.Left, 50)
+        canShootDKzeras = false
     }
 })
 function Crash () {
-    if (Bullet.isTouching(Enemy)) {
+    if (BulletDkzeras.isTouching(EnemyDkzeras)) {
         game.addScore(1)
-        Bullet.delete()
-        Enemy.delete()
-        Enemy = game.createSprite(randint(0, 4), 0)
-        canShoot = true
-    } else if (Bullet.get(LedSpriteProperty.Y) == 0) {
-        Bullet.delete()
-        canShoot = true
+        BulletDkzeras.delete()
+        EnemyDkzeras.delete()
+        EnemyDkzeras = game.createSprite(randint(0, 4), 0)
+        canShootDKzeras = true
+    } else if (BulletDkzeras.get(LedSpriteProperty.Y) == 0) {
+        BulletDkzeras.delete()
+        canShootDKzeras = true
     }
 }
-let Bullet: game.LedSprite = null
-let canShoot = false
-let Enemy: game.LedSprite = null
-let Spaceship: game.LedSprite = null
-Spaceship = game.createSprite(2, 4)
-Enemy = game.createSprite(randint(0, 4), 0)
-canShoot = true
+let BulletDkzeras: game.LedSprite = null
+let canShootDKzeras = false
+let EnemyDkzeras: game.LedSprite = null
+let SpaceshipDkzeras: game.LedSprite = null
+SpaceshipDkzeras = game.createSprite(2, 4)
+EnemyDkzeras = game.createSprite(randint(0, 4), 0)
+canShootDKzeras = true
 game.startCountdown(20000)
 basic.forever(function () {
-    Spaceship.move(1)
-    Spaceship.ifOnEdgeBounce()
-    if (Bullet) {
-        Bullet.move(1)
+    SpaceshipDkzeras.move(1)
+    SpaceshipDkzeras.ifOnEdgeBounce()
+    if (BulletDkzeras) {
+        BulletDkzeras.move(1)
         Crash()
     }
     basic.pause(200)
